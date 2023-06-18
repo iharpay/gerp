ced4a4491eae4871c96361785c42fa23d85d2b7f
Ayah Harper <136263768+iharpay@users.noreply.github.com>
2023-06-18 16:18:23 -0400

    Create stringProcessing.h

---
+++ stringProcessing.h
@@ -0,0 +1,16 @@
+/*
+Joshua Bernstein and Ayah Harper
+April 30
+File purpose: This is the decleration 
+of stripNonAlphaNum, which plays a huge
+role in our program because 
+it allows us to properly hash in
+words.
+
+*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool stripNonAlphaNum(string &input);
